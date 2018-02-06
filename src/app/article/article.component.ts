b05d482a3d71459372b72d8a9c4d56de34ab5453
Krzysztof Stanaszek <k.stanaszek@gmail.com>
2018-02-06 14:44:49 +0100

    first chapter

---
+++ src/app/article/article.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { HostBinding } from '@angular/core/src/metadata/directives';
+import { Article } from './article.model';
+
+@Component({
+  selector: 'app-article',
+  templateUrl: './article.component.html',
+  styleUrls: ['./article.component.css']
+})
+export class ArticleComponent implements OnInit {
+  @Input() article: Article;
+  @HostBinding('attr.class') cssClass = 'row';
+
+  constructor() {
+  }
+
+  voteUp(): boolean {
+    this.article.voteUp();
+    return false;
+  }
+  voteDown(): boolean {
+    this.article.voteDown();
+    return false;
+  }
+
+  ngOnInit() {
+  }
+}

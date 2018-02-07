44b081d1465c0b5c388af8c101407dda5ec11755
Krzysztof Stanaszek <k.stanaszek@gmail.com>
2018-02-07 20:47:48 +0100

    FormsControls and FormGroup added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { ArticleComponent } from './article/article.component';
 import { ArticleListComponent } from './article-list/article-list.component';
+import { FormSkuComponent } from './form-sku/form-sku.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ArticleComponent,
-    ArticleListComponent
+    ArticleListComponent,
+    FormSkuComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
     HttpModule
   ],
   providers: [],

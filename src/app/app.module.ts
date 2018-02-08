2e6da73cc869401d0500dee585a3ca7c03e56dc4
Krzysztof Stanaszek <k.stanaszek@gmail.com>
2018-02-08 08:46:09 +0100

    builder form added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import { AppComponent } from './app.component';
 import { ArticleComponent } from './article/article.component';
 import { ArticleListComponent } from './article-list/article-list.component';
 import { FormSkuComponent } from './form-sku/form-sku.component';
+import { FormSkuBuilderComponent } from './form-sku-builder/form-sku-builder.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ArticleComponent,
     ArticleListComponent,
-    FormSkuComponent
+    FormSkuComponent,
+    FormSkuBuilderComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/form-sku-builder/form-sku-builder.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup} from '@angular/forms';
+
+@Component({
+  selector: 'app-form-sku-builder',
+  templateUrl: './form-sku-builder.component.html',
+  styleUrls: ['./form-sku-builder.component.css']
+})
+export class FormSkuBuilderComponent implements OnInit {
+
+  myForm: FormGroup;
+
+  constructor(fb: FormBuilder) {
+    this.myForm = fb.group({
+      'sku': ['ABC123']
+    });
+   }
+
+  ngOnInit() {
+  }
+
+  onSubmit(value: string): void {
+    console.log('you submitte value: ', value);
+  }
+}

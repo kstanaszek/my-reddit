7c9964255e84b17d6b997826ebd42081d14382e2
Krzysztof Stanaszek <kstanaszek@objectivity.co.uk>
2018-02-09 17:20:49 +0100

    two way data binding for sku added

--- src/app/form-sku-builder/form-sku-builder.component.ts
+++ src/app/form-sku-builder/form-sku-builder.component.ts
@@ -1,10 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators,FormControl } from '@angular/forms';
+import { Component, OnInit, EventEmitter } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
 import { AbstractControl } from '@angular/forms/src/model';
 
-function skuValidator(control: FormControl): {[s: string]: boolean} {
-  if(!control.value.match(/^123/)){
-    return {invalidSku: true}
+function skuValidator(control: FormControl): { [s: string]: boolean } {
+  if (!control.value.match(/^123/)) {
+    return { invalidSku: true };
   }
 }
 
@@ -16,13 +16,25 @@ function skuValidator(control: FormControl): {[s: string]: boolean} {
 export class FormSkuBuilderComponent implements OnInit {
 
   myForm: FormGroup;
-  //sku: AbstractControl;
+  sku: AbstractControl;
 
   constructor(fb: FormBuilder) {
     this.myForm = fb.group({
       'sku': ['ABC123', Validators.compose([Validators.required, skuValidator])]
     });
-    //this.sku = this.myForm.controls['sku'];
+    this.sku = this.myForm.controls['sku'];
+
+    this.sku.valueChanges.subscribe(
+      (value: string) => {
+        console.log('sku changed to: ', value);
+      }
+    );
+
+    this.myForm.valueChanges.subscribe(
+      (form: any) => {
+        console.log('form changed to', form)
+      }
+    );
   }
 
   ngOnInit() {
